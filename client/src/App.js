eb9d719a91a9f7f5a236b9e6faf817146b4e6cb7
sam west <samwest0230@gmail.com>
2021-07-13 14:13:43 -0400

    conversion api call added

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import './App.scss';
 import axios from 'axios'
 import Starting from "./components/startingCurrency"
 import End from "./components/endCurrency"
-const url = 'http://api.exchangeratesapi.io/v1/latest';
+const url = 'http://api.exchangeratesapi.io/v1/';
 const access_key = 'ffb50f324c1ac8ef64c0d035e489dc8c';
 
 
@@ -27,16 +27,29 @@ class App extends React.Component {
     }
 
   componentDidUpdate = (prevState) => {
+    if (this.state.fromAmount === 0){
+      return;
+    }
     const fromCurrency = this.state.fromCurrency;
-    const prevCurrency = prevState.fromCurrency;
+    const prevFromCurrency = prevState.fromCurrency;
+    const toCurrency = this.state.toCurrency;
+    const prevToCurrency = prevState.toCurrency;
+    const amount = this.state.fromAmount
     if 
-      (fromCurrency !== prevCurrency) {
-      console.log('new');
-
+      (fromCurrency !== prevFromCurrency || toCurrency !== prevToCurrency ) {
+        axios.get(url + 'convert' + '?access_key=' + access_key + '&from='+ 'CAD' + '&to='+ 'USD' + '&amount=' + '25')
+        .then(response => {
+          console.log(response)
+          this.setState({
+          })
+        })
+        .catch((err) => {
+          console.log(err);
+        })
       } 
   }
   componentDidMount = () => {
-    axios.get(url + '?access_key=' + access_key)
+    axios.get(url + 'latest' + '?access_key=' + access_key)
       .then(response => {
         console.log(response)
         this.setState({
